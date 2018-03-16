64ff4392a8b345837f4805b644ff6bbb89758ca8
ohtukasyota <syota.jaska@gmail.com>
2018-03-16 14:57:52 +0900

    タスク追加するフォーム作成

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,6 +6,24 @@ import './body.html';
 
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    return Tasks.find({}, { sort: { createdAt: -1} });
   }
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    if (text != '') {
+      Tasks.insert({
+        text,
+        createdAt: new Date(),
+      });
+  
+      target.text.value = '';
+    }
+  },
 });
